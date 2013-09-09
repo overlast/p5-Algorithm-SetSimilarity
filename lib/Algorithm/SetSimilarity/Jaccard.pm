f22913d43ad6d0a9574fcfaa391ae7bf98d11c6e
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-09-09 19:22:06 +0900

    implemented get_similarity()

--- lib/Algorithm/SetSimilarity/Jaccard.pm
+++ lib/Algorithm/SetSimilarity/Jaccard.pm
@@ -9,13 +9,35 @@ our $VERSION = "0.01";
 sub new {
     my ($class, $param) = @_;
     my %hash = ();
+    $hash{"is_sorted"} = $param->{is_sorted} if (exists $param->{is_sorted});
     bless \%hash, $class;
 }
 
 sub get_similarity {
     my ($self, $set1, $set2) = @_;
     my $score = -1.0;
-    if ((@$set1) && (@$set2)) {
+    if ((ref $set1 eq "ARRAY") && (ref $set2 eq "ARRAY") && (@$set1) && (@$set2)) {
+        unless ($self->{is_sorted} ) {
+            @{$set1} = sort {$a cmp $b} @$set1;
+            @{$set2} = sort {$a cmp $b} @$set2;
+        }
+        my $min_att = $#$set1;
+        $min_att = $#$set2 if ($min_att > $#$set2);
+        my ($att1, $att2) = (0, 0);
+        my $match_num = 0;
+        for (my ($att1, $att2) = (0, 0); ($att1 <= $min_att) && ($att2 <= $min_att);) {
+            if (($set1->[$att1] cmp $set2->[$att2]) == -1) {
+                $att1++;
+            } elsif (($set1->[$att1] cmp $set2->[$att2]) == 1) {
+                $att2++;
+            } else {
+                $match_num++;
+                $att1++;
+                $att2++;
+            }
+        }
+        my $diff_num = abs(($#$set1 + 1) - ($#$set2 + 1));
+        $score = $match_num / ($match_num + $diff_num);
     }
     return $score;
 }
