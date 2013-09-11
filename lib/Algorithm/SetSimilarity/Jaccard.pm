b423d53af3caa84483b70937d02d7fecd7efc59d
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-09-11 15:45:27 +0900

    implement filt_by_threshold() to filt using the threshold value

--- lib/Algorithm/SetSimilarity/Jaccard.pm
+++ lib/Algorithm/SetSimilarity/Jaccard.pm
@@ -55,19 +55,66 @@ sub _swap_set_ascending_order {
     }
     return wantarray ? ($set1, $set2) : [$set1, $set2];
 }
-
-sub filt {
+use YAML;
+sub filt_by_threshold {
     my ($self, $set1, $set2, $threshold) = @_;
     my $score = -1.0;
     if ((ref $set1 eq "ARRAY") && (ref $set2 eq "ARRAY") &&
             (@$set1) && (@$set2) &&
-                ($threshold >= 0.0) && ($threshold <= 1.0)) {
+                ($threshold > 0.0) && ($threshold <= 1.0)) {
         for(my $r = 0; $r <= 0; $r++) {
-            last unless (((($#$set1 + 1) * $threshold) < $#$set2) && ((($#$set2 + 1) * $threshold) < $#$set1));
+            ($set1, $set2) = $self->_swap_set_ascending_order($set1, $set2);
+            my $s1 = $#$set1 + 1;
+            my $s2 = $#$set2 + 1;
 
-            my $alpha = 0;
-            for (my $i = 0; $i <= $alpha; $i++) {
+            last unless (($s2 * $threshold) <= $s1); #size filtering
+
+            unless ( $self->{is_sorted} ) {
+                @{$set1} = sort {$a cmp $b} @$set1;
+                @{$set2} = sort {$a cmp $b} @$set2;
             }
+
+            my $alpha = int($s1 - ($threshold / (1 + $threshold)) * ($s1 + $s2)) + 1;
+            my $match_num = 0;
+            my ($att1, $att2) = (0, 0);
+            while (($att1 < $alpha) && ($att2 < $alpha)) {
+                if (($set1->[$att1] cmp $set2->[$att2]) == -1) {
+                    $att1++;
+                } elsif (($set1->[$att1] cmp $set2->[$att2]) == 1) {
+                    $att2++;
+                } else {
+                    $match_num++;
+                    $att1++;
+                    $att2++;
+                }
+                my $min = ($s1 - $att1);
+                $min = ($s2 - $att2) if ($min > ($s2 - $att2));
+                if (($match_num) + $min < ($threshold / (1 + $threshold)) * ($s1 + $s2)) {
+                    $match_num = 0;
+                    last;
+                }
+            }
+            last unless ($match_num >= 1);
+            while (($att1 < $s1) && ($att2 < $s2)) {
+                if (($set1->[$att1] cmp $set2->[$att2]) == -1) {
+                    $att1++;
+                } elsif (($set1->[$att1] cmp $set2->[$att2]) == 1) {
+                    $att2++;
+                } else {
+                    $match_num++;
+                    $att1++;
+                    $att2++;
+                }
+                my $min = ($s1 - $att1);
+                $min = ($s2 - $att2) if ($min > ($s2 - $att2));
+                if (($match_num) + $min < ($threshold / (1 + $threshold)) * ($s1 + $s2)) {
+                    $match_num = 0;
+                    last;
+                }
+            }
+            my $diff_num = ($s1 - $match_num) + ($s2 - $match_num);
+            $score = $match_num / ($match_num + $diff_num);
+
         }
     }
     return $score;
